ffd25c30c1abddeab42888b25221aca3772b0f85
dimakryshtal <dima.krshl@gmail.com>
2023-01-13 17:09:41 +0200

    Initial Commit

---
+++ Food Delivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Delivery
+//
+//  Created by Dima Kryshtal on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
